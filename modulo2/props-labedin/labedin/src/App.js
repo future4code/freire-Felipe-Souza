d2bfacde1f9e6b2fa14f68a436f18b441429b96d
felipesouza <united.fss@gmail.com>
2022-06-07 16:51:40 -0300

    Resolução parcial exercício Labedin

---
+++ modulo2/props-labedin/labedin/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import foto from './img/foto.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem= { foto } 
+          nome="Felipe Souza" 
+          descricao="Oi, eu sou o Felipe. Sou aluno da Labenu. Adoro tirar soneca a tarde, xingar pessoas aleatoriamente no LoL e estudar sobre tecnologia no geral."
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className='card-pequeno'>
+        <CardPequeno
+        email="united.fss@gmail.com"
+        endereco="Rua Presidente Médice 22"/>
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="FreeLance" 
+          descricao="Designer gráfico e gerenciamento de mídias sociais" 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="INH" 
+          descricao="Recepcionista no Ilhéus North Hotel." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CardPequeno.css'
+
+function CardPequeno(props) {
+    return (
+        <div className="littlecard-container">
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default CardGrande;
