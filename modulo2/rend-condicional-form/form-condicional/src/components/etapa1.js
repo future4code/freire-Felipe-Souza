fea3c21a3f00e03e1e60058cc9e3ad5a53ad871d
felipesouza <united.fss@gmail.com>
2022-06-20 18:32:54 -0300

    Resolucao exercico Postman

---
+++ modulo2/rend-condicional-form/form-condicional/src/components/etapa1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class etapa1 extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>etapa 1</p>
+            </div>
+        )
+    }
+}
