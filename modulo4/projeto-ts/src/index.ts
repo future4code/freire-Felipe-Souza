3c9dc4942cf6f3616eff91586d6d950712a4c0ae
FelipeSouza <united.fss@gmail.com>
2022-08-14 21:52:09 -0300

    projeto inicial ts

---
+++ modulo4/projeto-ts/src/index.ts
@@ -0,0 +1,58 @@
+/*
+function exercicio1 (): string {
+let nome: string = "felipe"
+let dia: string = "20"
+let mes: string = "10"
+let ano: string = "1993"
+return (`Olá, me chamo ${nome}, nasci no dia ${dia}, do mês ${mes}, no ano de ${ano}`)
+}
+
+console.log(typeof exercicio1())
+
+-------------------------------------------------------------------------------------
+
+function exercicio2 (): number {
+    let numero: number = 10
+    return numero
+}
+
+console.log(typeof exercicio2())
+
+
+-------------------------------------------------------------------------------------
+
+
+enum NOME {
+	ANEIS="O senhor dos anéis",
+	MATRIX="The Matrix",
+	FRACOS="Onde os fracos não tem vez",
+	PODEROSO="O poderoso chefão",
+	SCARFACE="Scarface"
+}
+
+enum GENERO {
+	ACAO="ação",
+	DRAMA="drama",
+	COMEDIA="comédia",
+	ROMANCE="romance",
+	TERROR="terror"
+}
+
+enum ANO {
+    ANO1="1990",
+    ANO2="1993",
+    ANO3="2000",
+    ANO4="2022"
+}
+
+function exercicio3 (): any {
+const anoLancamento = ANO.ANO3
+const genero = GENERO.DRAMA
+const nome = NOME.FRACOS
+return `${nome}, ${genero}, ${anoLancamento}`
+}
+
+console.log(exercicio3())
+
+----------------------------------------------------------------------------------
+*/
