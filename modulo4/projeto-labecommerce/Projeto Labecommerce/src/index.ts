b5e0c736e9adaa640628f6fab6ffe096590f9a94
FelipeSouza <united.fss@gmail.com>
2022-09-01 23:08:52 -0300

    projeto labecommerce

---
+++ modulo4/projeto-labecommerce/Projeto Labecommerce/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express";
+import cors from "cors";
+import { AddressInfo } from "net";
+
+const app: Express = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if(server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Servidor rodando na porta http://localhost:${address.port}`)
+    } else {
+        console.error("Falha ao se conectar com o servidor")
+    }
+});
