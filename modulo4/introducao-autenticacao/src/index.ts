cf6cb94d42c3e0c1fb5ba21c673c9bddcd289f6e
FelipeSouza <united.fss@gmail.com>
2022-09-13 00:12:44 -0300

    exercicio

---
+++ modulo4/introducao-autenticacao/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { generateId } from "./services/generateId"
+
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use(express.json());
+
+import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});;
